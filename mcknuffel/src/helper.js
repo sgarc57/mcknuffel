2a9e490aa2db43c6738c02b3871e934721193148
steven garcia <steven.garcia@booking.com>
2024-01-28 13:13:22 +0100

    fixing some packages and adding product

--- mcknuffel/src/helper.js
+++ mcknuffel/src/helper.js
@@ -1,6 +1,7 @@
 import blueDino from './img/blue-dino-bronco.jpg';
 import seaTurtle from './img/sea-turtle.jpg';
 import greenTrex from './img/green-dino.jpg';
+import monsterFruit from './img/monster-fruit.jpg';
 
 // Define the categories
 const categories = ['big', 'small', 'animal', 'weird'];
@@ -44,9 +45,9 @@ export function Home({products}) {
   );
 }
 
-// Mock product data
+// product data
 export const products = [
   { id: 1, name: 'Great Green Sea Turtle', description: 'We bought this toy when we were at an aquarium in  New Zealand, the name of the aquarium was Kelly Tarltons', category: 'big', image: seaTurtle },
   { id: 2, name: 'Green T-Rex', description: 'We just got this one from Beckett when he was very small, he left it at our house in New Zealand', category: 'weird', image: greenTrex },
+  { id: 3, name: 'Monster Fruit', description: 'I really wanted a squishy but i the other ones where sold out so mom bought me this', category: 'weird', image: greenTrex },
 ];
-
